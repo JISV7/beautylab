0ca7b112b2c2629a5133d98cb3bd283339a752f1
JISV7 <139592416+JISV7@users.noreply.github.com>
2026-03-10 01:26:09 -0400

    ok

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -25,12 +25,12 @@ export const Dashboard: React.FC<DashboardProps> = ({ onNavigateToAdmin, onLogou
             onLogout={onLogout}
         >
             <div className="mx-auto p-6">
-                <h1>Hello World</h1>
-                <h2>Hello World</h2>
-                <h3>Hello World</h3>
-                <h4>Hello World</h4>
-                <h5>Hello World</h5>
-                <h6>Hello World</h6>
+                <h1 className="text-h1-size text-h1-color text-h1-font text-h1-weight">Hello World</h1>
+                <h2 className="text-h2-size text-h2-color text-h2-font text-h2-weight">Hello World</h2>
+                <h3 className="text-h3-size text-h3-color text-h3-font text-h3-weight">Hello World</h3>
+                <h4 className="text-h4-size text-h4-color text-h4-font text-h4-weight">Hello World</h4>
+                <h5 className="text-h5-size text-h5-color text-h5-font text-h5-weight">Hello World</h5>
+                <h6 className="text-h6-size text-h6-color text-h6-font text-h6-weight">Hello World</h6>
             </div>
         </DashboardLayout>
     );
